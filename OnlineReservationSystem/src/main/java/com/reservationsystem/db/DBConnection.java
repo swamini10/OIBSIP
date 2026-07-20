b1d69c762f49d33035e846cf7bd431b9eb906916
swamini <swaminibhandare10@gmail.com>
2026-07-20 14:33:11 +0530

    Add database connection utility

---
+++ OnlineReservationSystem/src/main/java/com/reservationsystem/db/DBConnection.java
@@ -0,0 +1,38 @@
+package com.reservationsystem.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/reservation_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root@123";
+
+    public static Connection getConnection() {
+        try {
+
+            Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("✅ Database Connected Successfully");
+            return con;
+
+        } catch (SQLException e) {
+
+            System.out.println("❌ Connection Failed");
+            e.printStackTrace();
+            return null;
+
+        }
+
+    }
+
+}
+
+/*
+ private ka use kele -> encapulation it use only with in class we can not access it outside the class
+ final key word ka use kele -> one time assign and aslo we cannot change it value  if value fix
+ method static ka banvali ahe -> object creat karychi garj padnar nahi apn direct class name ne call karu shakato
+ (localhost:3306/reservation_db) ya made localhost kay ahe ni 3306 kay ahe  ->  localhost meas local machine ani 3306 he mysql cha port no ahe
+
+
+*/
